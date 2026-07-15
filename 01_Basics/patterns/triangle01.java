492aae4417f054e0c5b6e8fc21646d27477c14ba
Nileswar007 <niledawn03@gmail.com>
2026-07-15 22:46:37 +0530

    Add 0-1 Triangle pattern implementation

---
+++ 01_Basics/patterns/triangle01.java
@@ -0,0 +1,17 @@
+public class triangle01 {
+    public static void main(String[] args){
+
+        for(int i=0; i<5; i++)
+        {
+            for(int j=0; j<i+1; j++){
+
+                if((i+j)%2==0)
+                System.out.print("1 ");
+                else
+                System.out.print("0 ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
